d837d8fd8f2eec0f4c4d422632007909130fc9f3
Mohammad Prince <mprince2k16@gmail.com>
2022-06-18 16:11:18 +0600

    first commit

--- balticsamurai/twilio/Helper.php
+++ balticsamurai/twilio/Helper.php
@@ -92,11 +92,11 @@ class Twilio {
         $result = $dispute->get_disputed($caller_sid);
         if($result)
         {
-            return 'true';
+            return 'Disputed';
         }
         else
         {
-            return 'false';
+            return 'Disput';
         }
     }
 
@@ -119,5 +119,70 @@ class Twilio {
     }
 }
 
-$twilio = new Twilio('ACfc5a0354828c3c703a2c9d06365d2d73', 'cc6d90f86ba1d37c79cd7a5cceeaadbb');
+$twilio = new Twilio('ACfc5a0354828c3c703a2c9d06365d2d73', '8551726162fb53956c659e773d42e7cd');
 $all_calls = json_decode($twilio->get_calls(), true);
+// $all_calls foreach loop in table
+
+$all_call = '';
+
+$i = 1;
+
+foreach ($all_calls['calls'] as $call){
+
+    if($twilio->check_disputed($call['sid']) == 'Disputed')
+    {
+        $result = $twilio->get_disputed_value($call['sid']);
+    }
+    else
+    {
+        $result = '<form action="store.php" method="POST">'.
+                    '<div class="mb-3">'.
+                        '<input type="hidden" name="caller_sid" value="'. $call['sid'] .'">'.
+                         '<label for="exampleInputEmail1" class="form-label">Write down the reason: </label>'.
+                           '<textarea require name="reason" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></textarea>'.
+                    '</div>'.
+                    '<div class="modal-footer">'.
+                        '<button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>'.
+                        '<button type="submit" class="btn btn-primary">Dispute</button>'.
+                    '</div>'.
+                '</form>';
+    }
+
+    $all_call .= '<tr>'.
+        '<td>' . $i++ . '</td>'.
+        '<td>' . $call["sid"] . '</td>'.
+        '<td>'.$call['from_formatted'].'</td>'.
+        '<td>'.$call['to_formatted'].'</td>'.
+        '<td>'.$call['duration'].'</td>'.
+        '<td>'.$call['start_time'].'</td>'.
+        '<td>'.$call['end_time'].'</td>'.
+        '<td>'.$call['status'].'</td>'.
+        '<td>'.
+            '<audio controls>'.
+                '<source src="'.$twilio->get_recording($call['sid']).'" type="audio/mpeg">'.
+                'Your browser does not support the audio element.'.
+            '</audio>'.
+        '</td>'.
+        '<td>'.
+            '<button type="button" class="btn btn-sm btn-secondary" data-bs-toggle="modal" data-bs-target="#'. $call['sid'] .'">'.
+                '<span class="badge badge-pill badge-danger">'. $twilio->check_disputed($call['sid']) .'</span>'.
+            '</button>'.
+
+            '<div class="modal fade" id="'. $call['sid'] .'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">'.
+                '<div class="modal-dialog" role="document">'.
+                    '<div class="modal-content">'.
+                        '<div class="modal-header">'.
+                            '<h5 class="modal-title" id="exampleModalLabel">'. $call['sid'] .'</h5>'.
+                            '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'.
+                                '<span aria-hidden="true">&times;</span>'.
+                            '</button>'.
+                        '</div>'.
+                        '<div class="modal-body">'.
+                            '<div>'. $result .'</div>'.
+                        '</div>'.
+                        
+                    '</div>'.
+                '</div>'.
+        '</td>'
+        ;
+}
---
+++ balticsamurai/twilio/get_all_call_logs.php
@@ -0,0 +1,6 @@
+<?php
+
+include('Helper.php');
+
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($all_call);
